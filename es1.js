60f49af550106156d8e766fcc922a81a4e651351
Sala Luca Andrea <sala.lucaandrea@osdbsesto>
2020-11-12 13:29:34 +0100

    commit

---
+++ es1.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", addBtn);
+var addBtn=function (){
+    var btn= document.createElement("button");
+    btn.innerHTML="NON CLICCARE";
+}
+
+var span1 = document.getElementById("span1");
+console.log("document: ", document);
+console.log("span1 ", span1);
+var testo = span1.innerHTML;
+console.log("testo ", testo);
+
+
+var button=document.getElementById("btn1");
+var changeColor=function(){
+    console.log("Buongiornissimo"); 
+    document.getElementById("div1").setAttribute("style", "background-color:  rgb(163, 36, 68)");
+    span1.innerHTML="CUCU";
+    document.getElementById("div1").innerHTML="<h2>Ti ho detto di non premerlo</h2>";
+}
+button.addEventListener("click", changeColor);
