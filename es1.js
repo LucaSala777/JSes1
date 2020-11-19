600b504f499023dd2f31ceeaa90980f609088c68
Sala Luca Andrea <sala.lucaandrea@osdbsesto>
2020-11-19 12:19:25 +0100

    commit

--- es1.js
+++ es1.js
@@ -1,26 +1,20 @@
-document.addEventListener("DOMContentLoaded", addBtn);
-var addBtn=function (){
-    var btn= document.createElement("button");
-    btn.innerHTML="NON CLICCARE";
-
+var addBtn = function(){
+    var btn = document.createElement("button");
+    btn.innerHTML = "NON CLICCARE";
+  
     document.getElementById("body1").append(btn);
-    button.addEventListener("click", addBtn);
-}
-
-var span1 = document.getElementById("span1");
-console.log("document: ", document);
-console.log("span1 ", span1);
-var testo = span1.innerHTML;
-console.log("testo ", testo);
-
-
-var button=document.getElementById("btn1");
-var changeColor=function(){
-    console.log("Buongiornissimo"); 
-    document.getElementById("div1").setAttribute("style", "background-color:  rgb(163, 36, 68)");
-    span1.innerHTML="CUCU";
-    document.getElementById("div1").innerHTML="<h2>Ti ho detto di non premerlo</h2>";
-};
-button.addEventListener("click", changeColor);
-
-
+  
+    btn.addEventListener("click", changeColor);
+  }
+  
+  var changeColor = function () {
+    console.log("BUONGIORNISSIMOOO!!1!!1");
+    document
+      .getElementById("div1")
+      .setAttribute("style", "background-color:   rgb(163, 36, 68)");
+    document.getElementById("span1").innerHTML = "CUCU";
+    document.getElementById("div1").innerHTML =
+      "<h1>TI AVEVO DETTO DI NON CLICCARE MALEDETTO</h1>";
+  };
+  
+  document.addEventListener("DOMContentLoaded", addBtn);
